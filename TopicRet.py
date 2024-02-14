2cb140fb4e999b8e328a9f76057e372223c32b9c
Omri Bloch <omri.bloch@gmail.com>
2024-02-14 11:51:04 +0000

    basic evaluation

--- TopicRet.py
+++ TopicRet.py
@@ -3,6 +3,7 @@ import re
 import os
 import argparse
 from tqdm import tqdm
+import numpy as np
 
 from model_loaders import load_pcw_wrapper
 from logits_processor import RestrictiveTokensLogitsProcessor
@@ -44,35 +45,66 @@ def get_samples(): # return a list of windows and a list of topics
             windows, topics = split_windows(txt)
             yield {'windows': windows, 'topics': topics}
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--just-one', action='store_true', help='infer the first and print')
-    parser.add_argument('--out', type=str, help='path to put results for evaluation')
-    parser.add_argument('--temp', type=float, help='sampling temperature, default to zero')
-    args = parser.parse_args()
-    
+
+def run(just_one=False, out=None, temp=None):
+    print(f'just one: {just_one}')
     wrapper = load_pcw_wrapper('TinyLlama/TinyLlama-1.1B-Chat-v1.0')
     outputs = []
     for sample in tqdm(get_samples()):
         windows = sample['windows']
         topics = sample['topics']
-        task_text= '<|user|>\nWhat topics did we just discussed?</s>'
-        do_sample = args.temp == 0
+        task_text= '<|user|>\nWhat topics did we just discussed? Use exact words.</s>'
+        do_sample = temp == 0
         # original instruction: '<|user|>\nWhat is the first topic we discussed? Only give me the topic name. Do not summarize yourself.</s>'
         output = wrapper.pcw_generate(contexts=windows,
                                     task_text=task_text,
-                                    temperature=args.temp,
+                                    temperature=temp,
                                     do_sample=do_sample,
                                     max_new_tokens=100)
         outputs.append(output)
-        if args.just_one:
+        if just_one:
             print(f"topics were:{os.linesep} {os.linesep.join(topics)}\n")
             print(f'model output is {output}')
             return
         
-        with open(args.out, 'w+') as f:
+        with open(out, 'w+') as f:
             for result in outputs:
-                f.write(result)
+                f.write(json.dumps({'topics': topics, 'result': result}) + '\n')
+
+def evaluate(results: str):
+    with open(results, 'r') as f:
+        recalls = []
+        for line in f.readlines():
+            sample = json.loads(line)
+            for topic in sample['topics']:
+                topic = topic.strip()
+                if topic in sample['result']:
+                    recalls.append(1)
+                    break
+            recalls.append(0)
+    print(f'average recall is {np.mean(recalls)}')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers()
+    run_parserr = subparser.add_parser('run')
+    run_parserr.add_argument('--just-one', action='store_true', help='infer the first and print')
+    run_parserr.add_argument('--out', type=str, help='path to put results for evaluation')
+    run_parserr.add_argument('--temp', type=float, help='sampling temperature, default to zero')
+    run_parserr.set_defaults(func=run)
+    
+    eval_parser = subparser.add_parser('eval')
+    eval_parser.add_argument('results', type=str, help='results file')
+    eval_parser.set_defaults(func=evaluate)
+    
+    args = parser.parse_args()
+    arguments_dict = dict(**vars(args))
+    del arguments_dict['func']  
+    print(f'arguments dict {arguments_dict}')
+    args.func(**arguments_dict)
+    
+    
 
 if __name__ == '__main__':
     main()
